71d3d02f3dafcd172d24669409200320c57cd10d
Paulo Pina <paulo.s.pina@noesis.pt>
2023-10-31 12:09:14 +0000

    Other entities and second migration

---
+++ SalesWebMvc/Models/Enums/SaleStatus.cs
@@ -0,0 +1,16 @@
+﻿namespace SalesWebMvc.Models
+{
+    public enum SaleStatus
+    {
+        Pending = 0,
+        Billed = 1,
+        Canceled = 2
+
+        //Pode ser feito assim:
+        /*
+        PENDING,
+        BILLED,
+        CANCELED
+        */
+    }
+}
