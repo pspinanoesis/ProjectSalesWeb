09df01ff48bd83df01a01c37500c0f5b93c286c1
Paulo Pina <paulo.s.pina@noesis.pt>
2023-11-13 12:35:30 +0000

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -8,16 +8,24 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength( 60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")] //{0} é o atributo que sera Name, {1} é o MinimumLength, {2} é o MaxLength que é 60
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} is required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]//aqui {0} é o atributo  que sera Email
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} is required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} is required")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
